85c016987d7625c5fbc1feeb526e979ab38ebe36
rarojas25 <rocio.rojas02@alumnos.ucn.cl>
2026-09-17 20:47:27 -0300

    Agregar menu principal y validacion basica de opciones

--- Taller/logica/Main.java
+++ Taller/logica/Main.java
@@ -1,8 +1,31 @@
 package logica;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	static Scanner teclado = new Scanner(System.in);
+	
 	public static void main(String[]args) {
 		int opcion;
+		do {
+			mostrarMenuPrincipal();
+			opcion = leerOpcionMenu(1,7);
+			switch (opcion) {
+			
+			}
+		}
+	}
+
+	
+	
+	private static int leerOpcionMenu(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static void mostrarMenuPrincipal() {
+		// TODO Auto-generated method stub
 		
 	}
 }
